9ab06904dbfa16c9e4532109b4cd631d2ed14096
Nathan Faucett <nathanfaucett@gmail.com>
2021-02-27 11:50:47 -0500

    fix some bugs, make more data classes

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
-export { Builder, ShapeBuilder, ShapePoint, PlotSceneBuilder, } from "./builders";
+export { Builder, ShapeBuilder, PlotSceneBuilder } from "./builders";
 export { Arc, ArcManager, Axis, AxisManager, BoxPlot, BoxPlotManager, Grid, GridManager, Point, PointType, PointData, PointManager, Direction, Line, LineType, LineManager, Pointer, Plot, PlotManager, PlotSection, PointsPlot, F, FunctionPlot, } from "./components";
+export { IGetClosestPointTo, hasGetClosestPointTo } from "./IGetClosestPointTo";
